823dfb272d24718ba9387ac67699a5d3b2618c16
Reyad Mohamed Fathy <reyadmohamed631@gmail.com>
2025-10-09 12:27:20 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 // import Image from "next/image";
-import LandingPage from "@/components/website/landingPage/Landingpage"; 
+import Hero from "@/components/website/landingPage/Hero";
+import MarketOpportunityHighlight from "@/components/website/landingPage/MarketOpportunityHighlight";
+import MissionSection from "@/components/website/landingPage/MissionSection";
+import TestimonialsOrPartners from "@/components/website/landingPage/TestimonialsOrPartners";
+import ThreePillarsSnapshot from "@/components/website/landingPage/ThreePillarsSnapshot";
 export default function Home() {
   return (
     <div className="">
-      <LandingPage />
+      <>
+        <Hero backgroundUrl="/images/p2.jpg" />
+        <MissionSection />
+        <ThreePillarsSnapshot />
+        <MarketOpportunityHighlight />
+        <TestimonialsOrPartners />
+      </>
     </div>
   );
 }
--- components/website/landingPage/LandingPage.tsx
+++ components/website/landingPage/LandingPage.tsx
@@ -1,20 +1,13 @@
-import React from "react";
-import Hero from "./Hero";
-import MissionSection from "./MissionSection";
-import ThreePillarsSnapshot from "./ThreePillarsSnapshot";
-import MarketOpportunityHighlight from "./MarketOpportunityHighlight";
-import TestimonialsOrPartners from "./TestimonialsOrPartners";
-const LandingPage = () => {
-  return (
-    <>
-      <Hero backgroundUrl="/images/p2.jpg" />
-      {/* <MissionSection /> */}
-      <MissionSection />
-      <ThreePillarsSnapshot />
-      <MarketOpportunityHighlight />
-      <TestimonialsOrPartners />
-    </>
-  );
-};
+// import React from "react";
+// import Hero from "./Hero";
+// import MissionSection from "./MissionSection";
+// import ThreePillarsSnapshot from "./ThreePillarsSnapshot";
+// import MarketOpportunityHighlight from "./MarketOpportunityHighlight";
+// import TestimonialsOrPartners from "./TestimonialsOrPartners";
+// const LandingPage = () => {
+//   return (
+  
+//   );
+// };
 
-export default LandingPage;
+// export default LandingPage;
